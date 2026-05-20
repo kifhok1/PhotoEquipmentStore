ed53b99830700e92226d3682bf3252fe8a9feca2
ivan barysev <kifhok@gmail.com>
2026-05-20 19:27:22 +0300

    Add Reference add and edit form

---
+++ PhotoEquipmentStore.Desktop/Views/Pages/Admin/ReferenceAddView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace PhotoEquipmentStore.Views.Pages.Admin;
+
+public partial class ReferenceAddView : UserControl
+{
+    public ReferenceAddView()
+    {
+        InitializeComponent();
+    }
+}
