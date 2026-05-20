552d9d38de5709b9c838b76bac99c9661b0d7c6c
ivan barysev <kifhok@gmail.com>
2026-05-20 19:28:38 +0300

    Update user view form

--- PhotoEquipmentStore.Desktop/ViewModels/Pages/Admin/UsersViewModel.cs
+++ PhotoEquipmentStore.Desktop/ViewModels/Pages/Admin/UsersViewModel.cs
@@ -1,9 +1,55 @@
-namespace PhotoEquipmentStore.ViewModels.Pages;
+using System.Collections.ObjectModel;
+using Avalonia.Media.Imaging;
+using CommunityToolkit.Mvvm.Input;
+using PhotoEquipmentStore.Application.Services;
+using PhotoEquipmentStore.Helper;
+using PhotoEquipmentStore.Models;
 
-public class UsersViewModel : ViewModelBase
+namespace PhotoEquipmentStore.ViewModels.Pages.Admin;
+
+public partial class UsersViewModel : ViewModelBase
 {
-    public UsersViewModel()
+    public ObservableCollection<UserShow> Users
     {
+        get;
+        private set;
+    } = new();
+
+    private string usersCount;
+    public string UsersCount
+    {
+        get => usersCount;
+        set => usersCount = value;
+    }
+    
+    public ViewModelBase CurrentViewModel
+    {
+        get; 
+        set;
+    }
+    
+    public UsersViewModel(ViewModelBase currentViewModel)
+    {
+        CurrentViewModel = currentViewModel;
         
+        var usersDb = UsersService.GetUsers();
+        foreach (var user in usersDb)
+        {
+            Users.Add(new UserShow(
+                user.Id,
+                user.Name,
+                user.Login,
+                user.PhoneNumber,
+                user.Role, 
+                user.RoleID, 
+                BitmapHelper.FromBytes(user.Image)));
+        }
+        UsersCount = $"Количество элементов на форме: {Users.Count}";
+    }
+
+    [RelayCommand]
+    private void EditUser()
+    {
+        CurrentViewModel = new UserAddViewModel();
     }
 }
