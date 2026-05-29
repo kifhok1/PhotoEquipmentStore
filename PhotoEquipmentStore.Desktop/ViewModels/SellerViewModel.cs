21c1e178fd39b34862a1e016f296ac79e3bb8fc4
ivan barysev <kifhok@gmail.com>
2026-05-29 13:01:18 +0300

    adding an exit function to switch to the authorization form when idle on the form for 3 minutes

--- PhotoEquipmentStore.Desktop/ViewModels/SellerViewModel.cs
+++ PhotoEquipmentStore.Desktop/ViewModels/SellerViewModel.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Reactive;
+using System.Timers;
 using Avalonia.Media.Imaging;
+using Avalonia.Threading;
 using PhotoEquipmentStore.Models;
 using PhotoEquipmentStore.ViewModels.Pages.Seller;
 using ReactiveUI;
@@ -15,15 +17,19 @@ public class SellerViewModel : ViewModelBase
     private ViewModelBase _currentViewModel;
     private NavigationMenuItem _selectedNavigationMenuItem;
     private UserInfo _currentUser;
-    
+
+    // --- Таймер бездействия ---
+    private readonly Timer _inactivityTimer;
+    private const double InactivityTimeout = 3 * 60 * 1000; // 3 минуты
+
     public ReactiveCommand<Unit, Unit> LogoutCommand { get; }
-    
+
     public NavigationMenuItem SelectedNavigationMenuItem
     {
         get => _selectedNavigationMenuItem;
         set => this.RaiseAndSetIfChanged(ref _selectedNavigationMenuItem, value);
     }
-    
+
     public ObservableCollection<NavigationMenuItem> NavigationMenuItems { get; }
 
     public ViewModelBase CurrentViewModel
@@ -37,64 +43,54 @@ public class SellerViewModel : ViewModelBase
         get => _currentUser;
         set => this.RaiseAndSetIfChanged(ref _currentUser, value);
     }
-    
+
+    public void ResetInactivityTimer()
+    {
+        _inactivityTimer.Stop();
+        _inactivityTimer.Start();
+    }
+
+    private void OnInactivityElapsed(object? sender, ElapsedEventArgs e)
+    {
+        Dispatcher.UIThread.Post(() =>
+        {
+            _inactivityTimer.Stop();
+            _mainViewModel.GoToLoginCommand.Execute().Subscribe();
+        });
+    }
+
     public SellerViewModel(MainViewModel mainViewModel, UserInfo userInfo)
     {
         _mainViewModel = mainViewModel;
         LogoutCommand = ReactiveCommand.Create(Logout);
-        
-        // Инициализация команд навигации
-        ReactiveCommand<Unit, Unit> goToClientCommand = ReactiveCommand.Create(GoToClients);
+
+        ReactiveCommand<Unit, Unit> goToClientCommand    = ReactiveCommand.Create(GoToClients);
         ReactiveCommand<Unit, Unit> goToClientAddCommand = ReactiveCommand.Create(GoToAddClients);
-        ReactiveCommand<Unit, Unit> goToOrderCommand = ReactiveCommand.Create(GoToOrders);
-        ReactiveCommand<Unit, Unit> goToOrderAddCommand = ReactiveCommand.Create(GoToAddOrder);
-        
+        ReactiveCommand<Unit, Unit> goToOrderCommand     = ReactiveCommand.Create(GoToOrders);
+        ReactiveCommand<Unit, Unit> goToOrderAddCommand  = ReactiveCommand.Create(GoToAddOrder);
+
         NavigationMenuItems = new ObservableCollection<NavigationMenuItem>
         {
-            
-            new NavigationMenuItem("Клиенты", "Client",  goToClientCommand),
-            new NavigationMenuItem("Добавление клиента", "ClientAdd",  goToClientAddCommand),
-            new NavigationMenuItem("Заказы", "Order", goToOrderCommand),
-            new NavigationMenuItem("Создание заказа", "AddOrder", goToOrderAddCommand),
-        };
-        
-        _selectedNavigationMenuItem = NavigationMenuItems[0];
-        _currentViewModel = new ClientsViewModel(GoToEditClient); 
-        _currentUser = userInfo;
-    }
-    
-     // Конструктор для дизайнера
-    [Obsolete("Design-time only")]
-    public SellerViewModel()
-    {
-        // Инициализация для дизайна (без MainViewModel)
-        LogoutCommand = ReactiveCommand.Create(() => { }); // Пустая команда для дизайна
-        
-        // Создаем пустые команды для дизайнера
-        ReactiveCommand<Unit, Unit> goToClientCommand = ReactiveCommand.Create(GoToClients);
-        ReactiveCommand<Unit, Unit> goToClientAddCommand = ReactiveCommand.Create(GoToAddClients);
-        ReactiveCommand<Unit, Unit> goToOrderCommand = ReactiveCommand.Create(GoToOrders);
-        ReactiveCommand<Unit, Unit> goToOrderAddCommand = ReactiveCommand.Create(GoToAddOrder);
-        
-        NavigationMenuItems = new ObservableCollection<NavigationMenuItem>
-        {
-            
-            new NavigationMenuItem("Клиенты", "Client",  goToClientCommand),
-            new NavigationMenuItem("Добавление клиента", "ClientAdd",  goToClientAddCommand),
-            new NavigationMenuItem("Заказы", "Order", goToOrderCommand),
-            new NavigationMenuItem("Создание заказа", "OrderAdd", goToOrderAddCommand),
+            new NavigationMenuItem("Клиенты",            "Client",    goToClientCommand),
+            new NavigationMenuItem("Добавление клиента", "ClientAdd", goToClientAddCommand),
+            new NavigationMenuItem("Заказы",             "Order",     goToOrderCommand),
+            new NavigationMenuItem("Создание заказа",    "AddOrder",  goToOrderAddCommand),
         };
 
-        
         _selectedNavigationMenuItem = NavigationMenuItems[0];
-        _currentViewModel = new ClientsViewModel();
-        _currentUser = new UserInfo("Ианов Иван", "Админ",
-            new Bitmap("/Users/ivanbarysev/RiderProjects/PhotoEquipmentStore/PhotoEquipmentStore.Desktop/Assets/user-test.jpg"));
+        _currentViewModel = new ClientsViewModel(GoToEditClient);
+        _currentUser = userInfo;
+
+        // Запускаем таймер сразу при входе
+        _inactivityTimer = new Timer(InactivityTimeout) { AutoReset = false };
+        _inactivityTimer.Elapsed += OnInactivityElapsed;
+        _inactivityTimer.Start();
     }
-    
-    
+
+    // остальные методы без изменений...
     private void Logout()
     {
+        _inactivityTimer.Stop();
         _mainViewModel.GoToLoginCommand.Execute().Subscribe();
     }
 
@@ -126,7 +122,7 @@ public class SellerViewModel : ViewModelBase
         CurrentViewModel = new OrderAddViewModel();
         SelectedNavigationMenuItem = NavigationMenuItems.First(item => item.Title == "Создание заказа");
     }
-    
+
     private void GoToOrderItems(OrderShow order)
     {
         CurrentViewModel = new OrderItemsViewModel(order);
