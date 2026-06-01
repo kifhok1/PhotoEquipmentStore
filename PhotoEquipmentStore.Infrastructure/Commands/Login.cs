d5cd06bb6d536b41daa772697559a6aef5522208
ivan barysev <kifhok@gmail.com>
2026-06-01 20:22:58 +0300

    adding exception handling to functions for displaying unique phone numbers and logins

--- PhotoEquipmentStore.Infrastructure/Commands/Login.cs
+++ PhotoEquipmentStore.Infrastructure/Commands/Login.cs
@@ -1,5 +1,6 @@
 using MySql.Data.MySqlClient;
 using PhotoEquipmentStore.Infrastructure.Connection;
+using PhotoEquipmentStore.Infrastructure.Exceptions;
 
 namespace PhotoEquipmentStore.Infrastructure.Commands;
 
@@ -9,20 +10,30 @@ public class Login
 
     public List<string> GetAllLogins()
     {
-        var logins = new List<string>();
-
-        string query = "SELECT login FROM users;";  // или с WHERE is_deleted = 0
-
-        using var connection = new MySqlConnection(ConnString);
-        connection.Open();
-        using var command = new MySqlCommand(query, connection);
-        using var reader = command.ExecuteReader();
-
-        while (reader.Read())
+        try
         {
-            logins.Add(reader.GetString("login"));
-        }
+            var logins = new List<string>();
 
-        return logins;
+            const string query = "SELECT login FROM users WHERE is_deleted = 0;";
+
+            using var connection = new MySqlConnection(ConnString);
+            connection.Open();
+
+            using var command = new MySqlCommand(query, connection);
+            using var reader  = command.ExecuteReader();
+
+            while (reader.Read())
+                logins.Add(reader.GetString("login"));
+
+            return logins;
+        }
+        catch (MySqlException ex)
+        {
+            throw new DatabaseException("Ошибка при получении списка логинов.", ex);
+        }
+        catch (Exception ex)
+        {
+            throw new DatabaseException("Непредвиденная ошибка при получении списка логинов.", ex);
+        }
     }
 }
--- PhotoEquipmentStore.Infrastructure/Commands/Phone.cs
+++ PhotoEquipmentStore.Infrastructure/Commands/Phone.cs
@@ -1,4 +1,5 @@
 using MySql.Data.MySqlClient;
+using PhotoEquipmentStore.Infrastructure.Exceptions;
 using PhotoEquipmentStore.Infrastructure.Connection;
 
 namespace PhotoEquipmentStore.Infrastructure.Commands;
@@ -6,26 +7,36 @@ namespace PhotoEquipmentStore.Infrastructure.Commands;
 public class Phone
 {
     private static readonly string ConnString = ConnectionSettingsParser.Load().ToString();
-    
+
     public List<string> GetAllPhoneNumbers()
     {
-        var phones = new List<string>();
-
-        string query = @"
-        SELECT phone FROM users WHERE phone IS NOT NULL AND phone != ''
-        UNION ALL
-        SELECT phone FROM clients WHERE phone IS NOT NULL AND phone != '';";
-
-        using var connection = new MySqlConnection(ConnString);
-        connection.Open();
-        using var command = new MySqlCommand(query, connection);
-        using var reader = command.ExecuteReader();
-
-        while (reader.Read())
+        try
         {
-            phones.Add(reader.GetString("phone"));
-        }
+            var phones = new List<string>();
 
-        return phones;
+            const string query = @"
+                SELECT phone FROM users   WHERE phone IS NOT NULL AND phone != '' AND is_deleted = 0
+                UNION ALL
+                SELECT phone FROM clients WHERE phone IS NOT NULL AND phone != '' AND is_deleted = 0;";
+
+            using var connection = new MySqlConnection(ConnString);
+            connection.Open();
+
+            using var command = new MySqlCommand(query, connection);
+            using var reader  = command.ExecuteReader();
+
+            while (reader.Read())
+                phones.Add(reader.GetString("phone"));
+
+            return phones;
+        }
+        catch (MySqlException ex)
+        {
+            throw new DatabaseException("Ошибка при получении списка номеров телефонов.", ex);
+        }
+        catch (Exception ex)
+        {
+            throw new DatabaseException("Непредвиденная ошибка при получении списка номеров телефонов.", ex);
+        }
     }
 }
